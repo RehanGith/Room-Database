ef7d0628eb7dfbfac995fa0e7df222e033879667
RehanGith <chrehan1007@gmail.com>
2025-01-01 18:17:14 +0500

    Added queries such as deletion and created database instance in mainactivity

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.roomdatabase
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val db = DBHolder.getInstance(this)
+        findViewById<Button>(R.id.btInsertTable).setOnClickListener {
+            db.dao.deleteStudent(1)
+            db.dao.deleteStudent(2)
+            println( db.dao.getAllStudents())
+        }
 
     }
 }
--- app/src/main/java/com/example/roomdatabase/StudentDAO.kt
+++ app/src/main/java/com/example/roomdatabase/StudentDAO.kt
@@ -1,11 +1,16 @@
 package com.example.roomdatabase
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-
+@Dao
 interface StudentDAO {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertStudent(student: StudentTable)
     @Query("SELECT * FROM student")
     fun getAllStudents(): List<StudentTable>
+    @Query("Delete from student where id = :stud")
+    fun deleteStudent(stud : Int)
 }
